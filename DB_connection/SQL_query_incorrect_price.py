fbb0a5e7f217e397a6eb931986f1286237f051b4
Iborodin <soupwithwater@gmail.com>
2020-06-09 21:02:51 +0300

    exicution of SQL query and write it to dict

--- DB_connection/SQL_query_incorrect_price.py
+++ DB_connection/SQL_query_incorrect_price.py
@@ -14,10 +14,9 @@ def query_with_fetchmany():
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
+        result = dict()
 
         cursor.execute("SELECT DISTINCT p.code as 'Код товара', \
-                                pr_cross.p_price as 'cross цена',\
-                                pr_sale.p_price as 'sale цена',\
                                 ROUND(100 - (pr_sale.p_price / (pr_cross.p_price / 100)), 0) as 'скидка'\
                             FROM products as p\
                                 LEFT JOIN (SELECT * FROM pricerows WHERE DATE(pricerows.p_starttime) <= CURDATE() AND DATE(pricerows.p_endtime) >= CURDATE() AND p_detmirpricerowtype = '8796117008475') AS pr_cross ON p.code = pr_cross.p_productid \
@@ -27,6 +26,12 @@ def query_with_fetchmany():
 
         for row in iter_row(cursor, 10):
             print(row)
+            product = row[0]
+            sale = row[1]
+            result.update({product:sale})
+
+
+        #print (result)
 
     except Error as e:
         print(e)
@@ -35,5 +40,6 @@ def query_with_fetchmany():
         cursor.close()
         conn.close()
 
+    return result
 if __name__ == '__main__':
     query_with_fetchmany()
