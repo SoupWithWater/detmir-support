9f448fe1cbc3bcf19099d3600f61a9ae22076264
Iborodin <soupwithwater@gmail.com>
2020-06-10 03:24:17 +0300

    v 1.1 added stocks_finder and loging

--- main.py
+++ main.py
@@ -1,42 +1,16 @@
 from SQL_query.SQL_query_incorrect_price import query_with_fetchmany
-import urllib.request
+from stocks_finder import stocks_finder
 
 def incorrect_price_alarm():
-    #load products with sale > 90%
+
     sql_result = query_with_fetchmany()
-    print('Товары со скидкой более 90% выгружены из базы')
-    alert_products = []
+    print(f'Товары со скидкой более 90% выгружены из базы, всего {len(sql_result)}')
 
-    for key in sql_result:
-        product_id = str(sql_result.get(key))
-        #print(f'Поиск офлайн стоков по товару {product_id} в сервисах')
+    print('Проверяю наличие в cubic01')
+    alert_products = stocks_finder(**sql_result)
 
-        solr_response_http = urllib.request.urlopen(f'http://cubic01:8983/solr/detmir-delivery/select?fl=offline_stock&q=product_id:{product_id}&rows=200&wt=python')
-        solr_response = (solr_response_http.read()).decode('utf-8')
-        stock_str = solr_response[(solr_response.find('[')) + 2:-7]
-        stock_str = stock_str.replace('\n', '').replace(' ', '').replace("'", "").replace('{', '').replace('}', '')
-        offline_stock = stock_str.split(',')
-
-        for stock in offline_stock:
-            if stock != 'offline_stock:NoStock':
-                if (alert_products.count(key)) < 1:
-                    alert_products.append(key)
-                    print(f'У товара {product_id} найдены offline стоки')
-
-        solr_response_http = urllib.request.urlopen(f'http://cubic01:8983/solr/detmir-delivery/select?fl=online_stock&q=product_id:{product_id}&rows=200&wt=python')
-        solr_response = (solr_response_http.read()).decode('utf-8')
-        stock_str = solr_response[(solr_response.find('[')) + 2:-7]
-        stock_str = stock_str.replace('\n', '').replace(' ', '').replace("'", "").replace('{', '').replace('}', '')
-        online_stock = stock_str.split(',')
-
-        for stock in online_stock:
-            if stock != 'online_stock:NoStock':
-                if (alert_products.count(key)) < 1:
-                    alert_products.append(key)
-                    print(f'У товара {product_id} найдены online стоки')
-
-
-
+    print('Проверить корректность цены следующих товаров:')
+    print(alert_products)
 
 if __name__ == '__main__':
     incorrect_price_alarm()
