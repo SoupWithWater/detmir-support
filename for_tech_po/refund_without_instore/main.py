3941cdfd69bb7faf4de4d64d4b1edbeeddfc3b43
Iborodin <soupwithwater@gmail.com>
2020-06-11 01:30:31 +0300

    fix decode for terminal

--- for_tech_po/refund_without_instore/main.py
+++ for_tech_po/refund_without_instore/main.py
@@ -1,5 +1,40 @@
+from mysql.connector import MySQLConnection, Error, cursor
 from sql_query.courier_refund import sql_courier_refund
+from sql_query.express_refund import sql_express_refund
+from sql_query.logistpickup_refund import sql_logistpickup_refund
 from sql_query.list_to_string import list_to_string
+from sql_query.mysql_dbconfig import read_db_config
+from sql_query.iter_row import iter_row
 
-date_refund = '20200610'
-test = sql_courier_refund(date_refund)
+
+
+def refund():
+    try:
+
+        date_refund = '20200610'
+
+        print('Connecting to hbs-mysql02-prod [...', end='')
+        dbconfig = read_db_config()
+        print('...', end='')
+        conn = MySQLConnection(**dbconfig)
+        print('...', end='')
+        cursor = conn.cursor()
+        print('........]')
+        if conn.is_connected():
+            print('Connection established.')
+        else:
+            print('connection failed.')
+
+
+
+
+        date_refund = '20200610'
+        courier = sql_courier_refund(date_refund, cursor)
+        express = sql_express_refund(date_refund, cursor)
+        logistpickup = sql_logistpickup_refund(date_refund, cursor)
+
+
+    except Error as e:
+        print(e)
+if __name__ == '__main__':
+    refund()
--- for_tech_po/refund_without_instore/sql_query/courier_refund.py
+++ for_tech_po/refund_without_instore/sql_query/courier_refund.py
@@ -1,34 +1,11 @@
-from mysql.connector import MySQLConnection, Error
 from sql_query.mysql_dbconfig import read_db_config
 from sql_query.list_to_string import list_to_string
+from sql_query.iter_row import iter_row
 
-def iter_row(cursor, size=10):
-    while True:
-        rows = cursor.fetchmany(size)
-        if not rows:
-            break
-        for row in rows:
-            yield row
-if __name__ == '__main__':
-    iter_row()
+def sql_courier_refund(date_refund, cursor):
+        need_refund_all = []
+        refund_follow_on_accepted = []
 
-def sql_courier_refund(date_refund):
-    try:
-        print('Connecting to hbs-mysql02-prod [...', end='')
-        dbconfig = read_db_config()
-        print('...', end='')
-        conn = MySQLConnection(**dbconfig)
-        print('...', end='')
-        need_refund = []
-        print('...', end='')
-        cursor = conn.cursor()
-        print('........]')
-        if conn.is_connected():
-            print('Connection established.')
-        else:
-            print('connection failed.')
-
-        print('Выгружаю заказы по которым требуется возврат...')
         cursor.execute(f"SELECT DISTINCT o.code \
                             FROM orders AS o \
                                 JOIN enumerationvalueslp AS elp\
@@ -48,21 +25,18 @@ def sql_courier_refund(date_refund):
                             WHERE elp.LANGPK=8796093349920 \
                                 AND oelp.LANGPK=8796093349920\
                                 AND oelp.p_name='частичная комплектация'\
-                                AND o.createdTS>'20200520'\
+                                AND o.createdTS > '20200520'\
                                 AND p.code='card'\
-                                and d.code='logist' AND oh.p_description like '%%новый статус=Отгружен%%'\
-                                AND  date (oh.createdts) = '{date_refund}'\
+                                and d.code='logist'\
+                                AND oh.p_description like '%%новый статус=Отгружен%%'\
+                                AND date (oh.createdts) = '{date_refund}'\
                                 AND pte.p_transactionstatus ='CREATE_SUBSCRIPTION_ACCEPTED'")
-
-        need_refund_all = []
 
         for row in iter_row(cursor, 10):
             need_refund_all.extend(list(row))
 
-
         orders = list_to_string(need_refund_all)
 
-        print('Выгружаю заказы, по которым возврат произведен')
         cursor.execute(f"SELECT DISTINCT o.code \
                                     FROM orders AS o \
                                         JOIN enumerationvalueslp AS elp\
@@ -87,7 +61,6 @@ def sql_courier_refund(date_refund):
                                         AND pte.p_transactionstatus='REFUND_FOLLOW_ON_ACCEPTED'\
                                         and o.code IN ({orders})")
 
-        refund_follow_on_accepted = []
 
         for row in iter_row(cursor, 10):
             refund_follow_on_accepted.extend(list(row))
@@ -97,13 +70,6 @@ def sql_courier_refund(date_refund):
         for order in refund_follow_on_accepted:
             need_refund.remove(order)
 
-        print("Заказы, по которым требуется ручной возврат:")
-        for order in need_refund:
-            print(order.decode('utf-8'))
-
-    except Error as e:
-        print(e)
-
-    return need_refund
+        return need_refund
 if __name__ == '__main__':
     sql_courier_refund()
