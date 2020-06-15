328f5942210465ed1f212b1b4c80f66d92b43592
Iborodin <soupwithwater@gmail.com>
2020-06-15 12:21:34 +0300

    fix decode

--- for_tech_po/refund_without_instore/sql_query/list_to_string.py
+++ for_tech_po/refund_without_instore/sql_query/list_to_string.py
@@ -3,7 +3,8 @@ def list_to_string(list):
     returned_string = ''
 
     for string in list:
-        returned_string = returned_string + ("'" + string.decode('utf-8') + "',")
+        print(string)
+        returned_string = returned_string + ("'" + string + "',")
 
     returned_string = returned_string[0:-1]
     return returned_string
