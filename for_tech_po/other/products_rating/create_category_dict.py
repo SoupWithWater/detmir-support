923490c110b4b0a73f0b6e6d2d449e1c7dd26367
Iborodin <soupwithwater@gmail.com>
2020-06-18 13:49:08 +0300

    product_rating optimisation

--- for_tech_po/other/products_rating/create_category_dict.py
+++ for_tech_po/other/products_rating/create_category_dict.py
@@ -1,5 +1,13 @@
-import json
 import urllib.request
 
+def get_category_list():
+    solr_request = urllib.request.urlopen('http://cubic01:8983/solr/detmir-categories/select?fl=category_id,%20category_level,%20category_name&q=*:*&rows=6000&wt=csv')
+    solr_request = (solr_request.read()).decode('utf-8')
+    solr_request = solr_request.split('\n')
 
-solr_request = urllib.request.urlopen('http://cubic01:8983/solr/detmir-categories/select?fl=category_id,category_level,category_name&q=*:*')
+    category_list = []
+    for category in solr_request:
+        category = category.split(',')
+        category_list.append(category)
+
+    return category_list
--- for_tech_po/other/products_rating/main.py
+++ for_tech_po/other/products_rating/main.py
@@ -1,12 +1,17 @@
 import urllib.request
 import json
 from parsing_data import parsing
+from create_category_dict import get_category_list
+from create_brand_list import get_brand_list
 
 # выгрузка товаров с рейтингом < 3, на выходе получаем список товаров (products_list)
 webUrl = urllib.request.urlopen('http://cubic01:8983/solr/detmir-products/select?fl=product_code&q=product_rating:%20[1%20TO%203]&rows=5000&wt=python')
 products_list = (webUrl.read()).decode('utf-8')
 products_list = products_list[(products_list.find(':[')) + 9:(products_list.find('  }}') - 2)].replace("        'product_code':'", "").replace("      {", "").replace("'}", '').replace("\n", '').replace("{", '')
 products_list = products_list.split(',')
+
+category_list = get_category_list()
+brand_list = get_brand_list()
 
 #запускается цикл: к каждому товару подтягиваются необходимые данные
 for product in products_list:
@@ -17,23 +22,22 @@ for product in products_list:
     main_category = ''
     level_two_category = ''
     for category in product_response["product_all_categories"]:
-        category_data = urllib.request.urlopen(f'http://cubic01:8983/solr/detmir-categories/select?fl=category_name_txt_ru,category_level&q=category_id:{product}')
-        category_data = parsing(category_data)
 
-        if category_data != '':
-            if category == product_response['product_category_primary']:
-                main_category = category_data['category_name_txt_ru']
+        for category_data in category_list:
+            if str(category_data[0]) == str(category):
+                if (str(category_data[1]) == '2') & (str(level_two_category) == ''):
+                    level_two_category = category_data[2]
 
-            if (str(category_data['category_level']) == '2') & (level_two_category == ''):
-                    level_two_category = category_data['category_name_txt_ru']
+                if str(category_data[0]) == str(product_response['product_category_primary']):
+                    main_category = category_data[2]
+
 
     print(main_category, ',', level_two_category, ',', end='')
 
-    brand_data = urllib.request.urlopen(f'http://cubic01:8983/solr/detmir-brands/select?fl=name&q=id:{product_response["product_brand"]}')
-    brand_data = parsing(brand_data)
-    if brand_data != '':
-        brand = brand_data["name"]
-    else: brand = '0'
+    brand = ''
+    for brands in brand_list:
+        if str(brands[0]) == str(product_response['product_brand']):
+            brand = brands[1]
 
     print(product_response['product_article'], ',', brand, ',', round(product_response['product_rating'], 1))
 
