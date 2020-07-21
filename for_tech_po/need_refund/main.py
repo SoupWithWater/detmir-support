f7a8b8bf84f4a9a6d845d0b4aa7cb6d3fe3ad944
Iborodin <soupwithwater@gmail.com>
2020-07-21 15:22:13 +0300

    instore refund

--- for_tech_po/need_refund/main.py
+++ for_tech_po/need_refund/main.py
@@ -1,7 +1,7 @@
 from mysql.connector import MySQLConnection, Error, cursor
 from sql_query.courier_refund import sql_courier_refund
 from sql_query.express_refund import sql_express_refund
-from sql_query.instore_refund import sql_instore_refund
+from sql_query.instore_refund_new import sql_instore_refund
 from sql_query.logistpickup_refund import sql_logistpickup_refund
 from sql_query.lastmile_refund import sql_lastmile_refund
 from sql_query.storepickup_refund import sql_storepickup_refund
@@ -29,7 +29,7 @@ def refund():
         print('Выгружаю заказы \n')
 
 
-        courier = sql_courier_refund(date_refund, cursor)
+        """courier = sql_courier_refund(date_refund, cursor)
         print('[LOGIST]')
         if courier != []:
             for order in courier:
@@ -46,7 +46,7 @@ def refund():
                 print(str(order))
         else: print('_______')
         print()
-        cursor = cursor.close()
+        cursor = cursor.close()"""
 
         cursor = conn.cursor()
 
@@ -92,6 +92,7 @@ def refund():
             print('_______')
         print()
         print('Выгружено')
+
 
     except Error as e:
         print(e)
