15a3593e8fa47fb85a5a8ae9933bfd56243dbe4e
Iborodin <soupwithwater@gmail.com>
2020-10-05 13:50:26 +0300

    update solr URLs

--- for_tech_po/other/products_rating/create_category_dict.py
+++ for_tech_po/other/products_rating/create_category_dict.py
@@ -1,7 +1,7 @@
 import urllib.request
 
 def get_category_list():
-    solr_request = urllib.request.urlopen('http://cubic01:8983/solr/detmir-categories/select?fl=category_id,%20category_level,%20category_name&q=*:*&rows=6000&wt=csv')
+    solr_request = urllib.request.urlopen('http://cubic-solr-01:8983/solr/detmir-categories/select?fl=category_id,%20category_level,%20category_name&q=*:*&rows=6000&wt=csv')
     solr_request = (solr_request.read()).decode('utf-8')
     solr_request = solr_request.split('\n')
 
